5f2e08d5cf896a050784fdd78979c93c58f92471
5jisoo <56earls@gmail.com>
2023-11-11 17:32:14 +0900

    Add: Member MVC

---
+++ talent-stock/src/main/java/com/second/talentstock/Member/domain/Member.java
@@ -0,0 +1,34 @@
+package com.second.talentstock.Member.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String loginID;
+
+    private String loginPW;
+
+    @Enumerated(STRING)
+    private MemberType userType;
+
+    private String name;
+
+    private String email;
+}
