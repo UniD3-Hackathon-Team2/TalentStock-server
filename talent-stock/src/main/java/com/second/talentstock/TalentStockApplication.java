61c5120cbba2f278f8bddbcbf7603020e7359151
5jisoo <56earls@gmail.com>
2023-11-11 16:46:19 +0900

    Add: initialize project

---
+++ talent-stock/src/main/java/com/second/talentstock/TalentStockApplication.java
@@ -0,0 +1,13 @@
+package com.second.talentstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentStockApplication.class, args);
+	}
+
+}
---
+++ talent-stock/src/test/java/com/second/talentstock/TalentStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.second.talentstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
